78f3a0b7d32d559a93b9bafda73ec035c3329408
Noriyuki Ishida <noriyuki106@gmail.com>
2017-09-02 10:16:58 +0900

    generalize NumericFunction to multivariable

--- noripi/src/main/kotlin/io/github/noriyuki106/main/Chapter4.kt
+++ noripi/src/main/kotlin/io/github/noriyuki106/main/Chapter4.kt
@@ -16,11 +16,12 @@
  */
 package io.github.noriyuki106.main
 
-import io.github.noriyuki106.numkt.function.draw
 import io.github.noriyuki106.neural_network.crossEntropyError
 import io.github.noriyuki106.neural_network.meanSquaredError
+import io.github.noriyuki106.numkt.NumericArray
 import io.github.noriyuki106.numkt.function.curryByFirst
 import io.github.noriyuki106.numkt.function.curryBySecond
+import io.github.noriyuki106.numkt.function.draw
 import io.github.noriyuki106.numkt.function.numericalDiff
 import io.github.noriyuki106.numkt.function.numericalGradient
 import io.github.noriyuki106.numkt.narrayOf
@@ -31,6 +32,7 @@ fun main(args: Array<String>) {
 //    sample4_3_2()
 //    sample4_3_3()
     sample4_4()
+    sample4_4_1()
 }
 
 private fun sample4_2_1() {
@@ -53,7 +55,7 @@ private fun sample4_2_2() {
 }
 
 private fun sample4_3_2() {
-    val function = fun (x: Double): Double = 0.01 * x * x + 0.1 * x
+    val function = fun(x: Double): Double = 0.01 * x * x + 0.1 * x
 
     function.draw(0.0..20.0)
     println(function.numericalDiff(5.0))
@@ -61,16 +63,26 @@ private fun sample4_3_2() {
 }
 
 private fun sample4_3_3() {
-    val function = fun (x1: Double, x2: Double): Double = x1 * x1 + x2 * x2
+    val function = fun(x1: Double, x2: Double): Double = x1 * x1 + x2 * x2
 
     println(function.curryBySecond()(4.0).numericalDiff(3.0))
     println(function.curryByFirst()(3.0).numericalDiff(4.0))
 }
 
 private fun sample4_4() {
-    val function = fun (x1: Double, x2: Double): Double = x1 * x1 + x2 * x2
+    val function = fun(x1: Double, x2: Double): Double = x1 * x1 + x2 * x2
 
     println(function.numericalGradient(narrayOf(3.0, 4.0)))
     println(function.numericalGradient(narrayOf(0.0, 2.0)))
     println(function.numericalGradient(narrayOf(3.0, 0.0)))
+}
+
+private fun sample4_4_1() {
+    val function = fun(x: NumericArray): Double = x * x
+
+    println(function.numericalGradient(narrayOf(3.0, 4.0)))
+    println(function.numericalGradient(narrayOf(0.0, 2.0)))
+    println(function.numericalGradient(narrayOf(3.0, 0.0)))
+
+//    println(function.applyGradientMethod()
 }
--- noripi/src/main/kotlin/io/github/noriyuki106/numkt/NumericArray.kt
+++ noripi/src/main/kotlin/io/github/noriyuki106/numkt/NumericArray.kt
@@ -18,40 +18,62 @@ class NumericArray(private val values: DoubleArray) {
         if (this.length != that.length) throw IllegalArgumentException()
         return narrayOf(*this.values.mapIndexed { idx, v -> v + that.values[idx] }.toDoubleArray())
     }
-    operator fun plus(that: Double): NumericArray = narrayOf(*this.values.map { it + that }.toDoubleArray())
-    operator fun plus(that: Int): NumericArray = narrayOf(*this.values.map { it + that }.toDoubleArray())
+
+    operator fun plus(that: Double): NumericArray = narrayOf(
+            *this.values.map { it + that }.toDoubleArray())
+
+    operator fun plus(that: Int): NumericArray = narrayOf(
+            *this.values.map { it + that }.toDoubleArray())
 
     operator fun minus(that: NumericArray): NumericArray {
         if (this.length != that.length) throw IllegalArgumentException()
         return narrayOf(*this.values.mapIndexed { idx, v -> v - that.values[idx] }.toDoubleArray())
     }
 
-    operator fun minus(that: Double): NumericArray = narrayOf(*this.values.map { it - that }.toDoubleArray())
-    operator fun minus(that: Int): NumericArray = narrayOf(*this.values.map { it - that }.toDoubleArray())
+    operator fun minus(that: Double): NumericArray = narrayOf(
+            *this.values.map { it - that }.toDoubleArray())
+
+    operator fun minus(that: Int): NumericArray = narrayOf(
+            *this.values.map { it - that }.toDoubleArray())
 
     operator fun times(that: NumericArray): Double {
         if (this.length != that.length) throw IllegalArgumentException()
         return this.values.mapIndexed { idx, v -> v * that.values[idx] }.sum()
     }
 
-    operator fun times(that: Double): NumericArray = narrayOf(*this.values.map { it * that }.toDoubleArray())
-    operator fun times(that: Int): NumericArray = narrayOf(*this.values.map { it * that }.toDoubleArray())
+    operator fun times(that: Double): NumericArray = narrayOf(
+            *this.values.map { it * that }.toDoubleArray())
+
+    operator fun times(that: Int): NumericArray = narrayOf(
+            *this.values.map { it * that }.toDoubleArray())
 
     operator fun div(that: NumericArray): NumericArray {
         if (this.length != that.length) throw IllegalArgumentException()
         return narrayOf(*this.values.mapIndexed { idx, v -> v / that.values[idx] }.toDoubleArray())
     }
 
-    operator fun div(that: Double): NumericArray = narrayOf(*this.values.map { it / that }.toDoubleArray())
-    operator fun div(that: Int): NumericArray = narrayOf(*this.values.map { it / that }.toDoubleArray())
+    operator fun div(that: Double): NumericArray = narrayOf(
+            *this.values.map { it / that }.toDoubleArray())
 
-    fun map(transformer: (Double) -> Double): NumericArray = narrayOf(*this.values.map { transformer(it) }.toDoubleArray())
-    fun mapIndexed(transformer: (Int, Double) -> Double): NumericArray = narrayOf(*this.values.mapIndexed { index, value -> transformer(index, value) }.toDoubleArray())
+    operator fun div(that: Int): NumericArray = narrayOf(
+            *this.values.map { it / that }.toDoubleArray())
+
+    fun map(transformer: (Double) -> Double): NumericArray = narrayOf(
+            *this.values.map { transformer(it) }.toDoubleArray())
+
+    fun mapIndexed(transformer: (Int, Double) -> Double): NumericArray = narrayOf(
+            *this.values.mapIndexed { index, value -> transformer(index, value) }.toDoubleArray())
 
     fun sum(): Double = this.values.sum()
     fun sumBy(transformer: (Double) -> Double): Double = this.values.sumByDouble { transformer(it) }
 
     fun max(): Double = this.values.max() ?: 0.0
+
+    fun removed(i: Int): NumericArray = narrayOf(*this.values.copyOfRange(0, i),
+            *this.values.copyOfRange(i + 1, this.length))
+
+    fun inserted(v: Double, i: Int): NumericArray = narrayOf(*this.values.copyOfRange(0, i),
+            v, *this.values.copyOfRange(i, this.length))
 }
 
 fun narrayOf(vararg values: Double) = NumericArray(values)
