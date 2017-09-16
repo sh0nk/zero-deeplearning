80e3ab8224514df52be9e23e4fb1d75e258ae2da
TakefumiOta <takefumi@retty.me>
2017-09-17 00:22:38 +0900

    2.3.3(ver.2)

--- van/src/chap2/main.py
+++ van/src/chap2/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import numpy as np
 
+def perceptron(x, w, b):
+  if np.sum(w*x) + b <= 0:
+    return 0
+  else:
+    return 1
+
 def AND(x1, x2):
   """
   simple logic circuit (AND)
@@ -17,11 +23,43 @@ def AND(x1, x2):
   x = np.array([x1, x2])
   w = np.array([0.5, 0.5])
   b = -0.7
-  tmp = np.sum(w*x) + b
-  if tmp <= 0:
-    return 0
-  else:
-    return 1
+  return perceptron(x, w, b)
+
+def NAND(x1, x2):
+  """
+  simple logic circuit (NAND)
+
+  >>> NAND(0, 0)
+  1
+  >>> NAND(1, 0)
+  1
+  >>> NAND(0, 1)
+  1
+  >>> NAND(1, 1)
+  0
+  """
+  x = np.array([x1, x2])
+  w = np.array([-0.5, -0.5])
+  b = 0.7
+  return perceptron(x, w, b)
+
+def OR(x1, x2):
+  """
+  simple logic circuit (OR)
+
+  >>> OR(0, 0)
+  0
+  >>> OR(1, 0)
+  1
+  >>> OR(0, 1)
+  1
+  >>> OR(1, 1)
+  1
+  """
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.2
+  return perceptron(x, w, b)
 
 def _test():
   import doctest
