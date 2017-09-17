534d6b70a8fb54884fb6f8f1a5b9f7f8cff203fb
TakefumiOta <takefumi@retty.me>
2017-09-18 02:54:52 +0900

    3.4.3

--- van/src/chap3/__init__.py
+++ van/src/chap3/__init__.py
@@ -1,9 +1,11 @@
-from . import activation_functions
-
 def suite():
+  from . import activation_functions
+  from . import network
+
   import doctest
   import unittest
   suite = unittest.TestSuite()
   suite.addTest(doctest.DocTestSuite(activation_functions))
+  suite.addTest(doctest.DocTestSuite(network))
   return suite
 
--- van/src/test/__init__.py
+++ van/src/test/__init__.py
@@ -3,6 +3,7 @@ import chap2
 import chap3
 
 def all_suite():
+
   suite = unittest.TestSuite()
   suite.addTests(chap2.suite())
   suite.addTests(chap3.suite())
