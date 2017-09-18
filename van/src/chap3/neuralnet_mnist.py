f49558eb7c491a0188f7e922e01a5619c4fac93e
TakefumiOta <takefumi@retty.me>
2017-09-18 17:18:27 +0900

    3.6.3

--- van/src/chap3/neuralnet_mnist.py
+++ van/src/chap3/neuralnet_mnist.py
@@ -79,6 +79,24 @@ def main():
 
   print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
 
+def main_with_batch():
+  """
+  predict with particular batch size
+  >>> main_with_batch()
+  Accuracy:0.9352
+  """
+  x, t = get_data()
+  network = init_network()
+  batch_size = 100
+  accuracy_cnt = 0
+  for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+  print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
 def _test():
   import doctest
   doctest.testmod()
