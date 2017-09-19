b4ed9104bf573453f31b821c41a1bd2c09cd1634
TakefumiOta <takefumi@retty.me>
2017-09-19 13:45:03 +0900

    4.2.2

--- van/src/chap4/loss_functions.py
+++ van/src/chap4/loss_functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def mean_squared_error(x, t):
+def mean_squared_error(y, t):
   """
   loss function
 
@@ -17,7 +17,29 @@ def mean_squared_error(x, t):
   >>> mean_squared_error(np.array(y), np.array(t))
   0.72250000000000003
   """
-  return 0.5 * np.sum((x - t) ** 2)
+  return 0.5 * np.sum((y - t) ** 2)
+
+def cross_entropy_error(y, t):
+  """
+  loss function(cross entropy)
+  if y is 0, np.log(y) will be -inf.
+  In this case, we cannot precise value because of this special num.
+  For preventing it, adding delta to y.
+
+  >>> t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0,]
+  >>> y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+  >>> cross_entropy_error(y, t)
+  Traceback (most recent call last):
+  ...
+  TypeError: can only concatenate list (not "float") to list
+  >>> cross_entropy_error(np.array(y), np.array(t))
+  0.51082545709933802
+  >>> y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0]
+  >>> cross_entropy_error(np.array(y), np.array(t))
+  2.3025840929945458
+  """
+  delta = 1e-7
+  return -np.sum(t * np.log(y + delta))
 
 def _test():
   import doctest
