df7869d90fb51f175af12307fb08668cea07a790
TakefumiOta <takefumi@retty.me>
2017-09-19 14:07:14 +0900

    4.2.4

--- van/src/chap4/loss_functions.py
+++ van/src/chap4/loss_functions.py
@@ -10,14 +10,22 @@ def mean_squared_error(y, t):
   >>> mean_squared_error(y, t)
   Traceback (most recent call last):
   ...
-  TypeError: unsupported operand type(s) for -: 'list' and 'list'
+  AttributeError: 'list' object has no attribute 'ndim'
   >>> mean_squared_error(np.array(y), np.array(t))
   0.097500000000000031
   >>> y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0]
   >>> mean_squared_error(np.array(y), np.array(t))
   0.72250000000000003
+  >>> y = [[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0], \
+           [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0]]
+  >>> mean_squared_error(np.array(y), np.array(t))
+  0.41000000000000003
   """
-  return 0.5 * np.sum((y - t) ** 2)
+  if y.ndim == 1:
+    y = y.reshape(1, y.size)
+    t = t.reshape(1, t.size)
+  batch_size = y.shape[0] 
+  return 0.5 * np.sum((y - t) ** 2) / batch_size
 
 def cross_entropy_error(y, t):
   """
@@ -31,15 +39,23 @@ def cross_entropy_error(y, t):
   >>> cross_entropy_error(y, t)
   Traceback (most recent call last):
   ...
-  TypeError: can only concatenate list (not "float") to list
+  AttributeError: 'list' object has no attribute 'ndim'
   >>> cross_entropy_error(np.array(y), np.array(t))
   0.51082545709933802
   >>> y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0]
   >>> cross_entropy_error(np.array(y), np.array(t))
   2.3025840929945458
+  >>> y = [[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0], \
+           [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0]]
+  >>> cross_entropy_error(np.array(y), np.array(t))
+  1.4067047750469419
   """
   delta = 1e-7
-  return -np.sum(t * np.log(y + delta))
+  if y.ndim == 1:
+    y = y.reshape(1, y.size)
+    t = t.reshape(1, t.size)
+  batch_size = y.shape[0] 
+  return -np.sum(t * np.log(y + delta)) / batch_size
 
 def _test():
   import doctest
