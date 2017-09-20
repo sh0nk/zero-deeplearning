0e804336bce04de43194e30b3713443736480fb2
TakefumiOta <takefumi@retty.me>
2017-09-20 10:28:13 +0900

    4.4.2

--- van/src/chap4/__init__.py
+++ van/src/chap4/__init__.py
@@ -1,11 +1,15 @@
 def suite():
   from . import loss_functions
   from . import numerical
+  from . import simplenet
+  from . import gradient
 
   import doctest
   import unittest
   suite = unittest.TestSuite()
   suite.addTest(doctest.DocTestSuite(loss_functions))
   suite.addTest(doctest.DocTestSuite(numerical))
+  suite.addTest(doctest.DocTestSuite(simplenet))
+  suite.addTest(doctest.DocTestSuite(gradient))
   return suite
 
