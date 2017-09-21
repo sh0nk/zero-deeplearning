a0d7f76879dffdac021df1b2837ec34cb0da6b87
TakefumiOta <takefumi@retty.me>
2017-09-21 16:51:53 +0900

    6.1.5

--- van/src/chap6/optimizers.py
+++ van/src/chap6/optimizers.py
@@ -14,9 +14,11 @@ class SGD:
     >>> params = {"test": np.array([[2.0, 1.0], [-2.0, 0.5]]), "test2": np.array([0.2, 0.4])}
     >>> grads = {"test": np.array([[0.5, 0.8], [1.0, 0.5]]), "test2": np.array([3.0, 1.5])}
     >>> SGD().update(params, grads)
-    >>> params
-    {'test': array([[ 1.995,  0.992],
-           [-2.01 ,  0.495]]), 'test2': array([ 0.17 ,  0.385])}
+    >>> params['test']
+    array([[ 1.995,  0.992],
+           [-2.01 ,  0.495]])
+    >>> params['test2']
+    array([ 0.17 ,  0.385])
     """
     for key in params:
       params[key] -= self.lr * grads[key]
@@ -39,9 +41,11 @@ class Momentum:
     >>> params = {"test": np.array([[2.0, 1.0], [-2.0, 0.5]]), "test2": np.array([0.2, 0.4])}
     >>> grads = {"test": np.array([[0.5, 0.8], [1.0, 0.5]]), "test2": np.array([3.0, 1.5])}
     >>> Momentum().update(params, grads)
-    >>> params
-    {'test': array([[ 1.995,  0.992],
-           [-2.01 ,  0.495]]), 'test2': array([ 0.17 ,  0.385])}
+    >>> params['test']
+    array([[ 1.995,  0.992],
+           [-2.01 ,  0.495]])
+    >>> params['test2']
+    array([ 0.17 ,  0.385])
     """
     if self.v is None:
       self.v = {}
@@ -51,6 +55,35 @@ class Momentum:
     for key in params.keys():
       self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
       params[key] += self.v[key]
+
+class AdaGrad:
+  def __init__(self, lr=0.01):
+    """
+    >>> opt = AdaGrad(lr=1.0)
+    >>> opt.lr
+    1.0
+    """
+    self.lr = lr
+    self.h = None
+
+  def update(self, params, grads):
+    """
+    >>> params = {"test": np.array([[2.0, 1.0], [-2.0, 0.5]]), "test2": np.array([0.2, 0.4])}
+    >>> grads = {"test": np.array([[0.5, 0.8], [1.0, 0.5]]), "test2": np.array([3.0, 1.5])}
+    >>> AdaGrad().update(params, grads)
+    >>> params['test']
+    array([[ 1.99,  0.99],
+           [-2.01,  0.49]])
+    >>> params['test2']
+    array([ 0.19,  0.39])
+    """
+    if self.h is None:
+      self.h = {}
+      for key, val in params.items():
+        self.h[key] = np.zeros_like(val)
+    for key in params.keys():
+      self.h[key] += grads[key] * grads[key]
+      params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
 
 def _test():
   import doctest
