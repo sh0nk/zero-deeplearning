d2eef5efa3ff7d1a49fd3c404700625c0389a0bd
TakefumiOta <takefumi@retty.me>
2017-09-22 20:25:14 +0900

    6.4.3

--- van/src/chap6/__init__.py
+++ van/src/chap6/__init__.py
@@ -1,11 +1,13 @@
 def suite():
   from . import optimizers
   from . import multi_layer_net
+  from . import dropout
 
   import doctest
   import unittest
   suite = unittest.TestSuite()
   suite.addTest(doctest.DocTestSuite(optimizers))
   suite.addTest(doctest.DocTestSuite(multi_layer_net))
+  suite.addTest(doctest.DocTestSuite(dropout))
   return suite
 
--- van/src/chap6/multi_layer_net.py
+++ van/src/chap6/multi_layer_net.py
@@ -3,12 +3,13 @@
 import numpy as np
 from chap4.gradient import numerical_gradient
 from chap5.layer_native import *
+from chap6.dropout import Dropout
 from collections import OrderedDict
 
 class MultiLayerNet:
-  def __init__(self, input_size, hidden_size_list, output_size, weight_decay_lambda=0.0):
+  def __init__(self, input_size, hidden_size_list, output_size, weight_decay_lambda=0.0, use_dropout=False, dropout_ratio=0.5):
     """
-    >>> net = MultiLayerNet(30, [20], 10)
+    >>> net = MultiLayerNet(30, [20], 10, use_dropout=True)
     >>> net.params["W1"].shape
     (30, 20)
     >>> net.params["b1"].shape
@@ -18,16 +19,21 @@ class MultiLayerNet:
     >>> net.params["b2"].shape
     (10,)
     >>> len(net.layers)
-    3
+    4
     >>> type(net.layers["Affine1"])
     <class 'chap5.layer_native.Affine'>
     >>> type(net.layers["Relu1"])
     <class 'chap5.layer_native.Relu'>
+    >>> type(net.layers["Dropout1"])
+    <class 'chap6.dropout.Dropout'>
     >>> type(net.layers["Affine2"])
     <class 'chap5.layer_native.Affine'>
     >>> type(net.lastLayer)
     <class 'chap5.layer_native.SoftmaxWithLoss'>
     """
+    self.weight_decay_lambda = weight_decay_lambda
+    self.use_dropout = use_dropout
+
     size_list = [input_size] + hidden_size_list + [output_size]
     self.params = {}
     for i in range(1, len(size_list)):
@@ -35,26 +41,40 @@ class MultiLayerNet:
       self.params["b{}".format(i)] = np.random.randn(size_list[i]) * np.sqrt(2 / size_list[i - 1])
 
     # layer
+    size_list = [input_size] + hidden_size_list
     self.layers = OrderedDict()
     for i in range(1, len(size_list)):
       self.layers["Affine{}".format(i)] = Affine(self.params["W{}".format(i)], self.params["b{}".format(i)])
-      if i + 1 < len(size_list):
-        self.layers["Relu{}".format(i)] = Relu()
-    self.lastLayer = SoftmaxWithLoss()
-    self.weight_decay_lambda = weight_decay_lambda
+      self.layers["Relu{}".format(i)] = Relu()
+      if self.use_dropout:
+        self.layers["Dropout{}".format(i)] = Dropout(dropout_ratio)
+    # add for output layer
+    i = len(size_list)
+    self.layers["Affine{}".format(i)] = Affine(self.params["W{}".format(i)], self.params["b{}".format(i)])
 
-  def predict(self, x):
+    self.lastLayer = SoftmaxWithLoss()
+
+  def predict(self, x, is_training=False):
     """
     >>> x = np.random.randn(10, 30)
     >>> net = MultiLayerNet(30, [20], 10)
     >>> net.predict(x).shape
     (10, 10)
+    >>> net = MultiLayerNet(30, [20], 10, use_dropout=True)
+    >>> net.predict(x).shape
+    (10, 10)
+    >>> net = MultiLayerNet(30, [20], 10, use_dropout=True)
+    >>> net.predict(x, is_training=True).shape
+    (10, 10)
     """
-    for layer in self.layers.values():
-      x = layer.forward(x)
+    for key, layer in self.layers.items():
+      if "Dropout" in key:
+        x = layer.forward(x, is_training)
+      else:
+        x = layer.forward(x)
     return x
 
-  def loss(self, x, t):
+  def loss(self, x, t, is_training=False):
     """
     >>> x = np.random.randn(10, 30)
     >>> t = np.random.randn(10, 10)
@@ -63,11 +83,11 @@ class MultiLayerNet:
     <class 'numpy.float64'>
     """
     weight_decay = 0.0
-    for i in range(1, int((len(self.layers) + 1) / 2) + 1):
+    for i in range(1, int(len(self.params) / 2) + 1):
       W = self.params["W{}".format(i)]
       weight_decay += np.sum(W * W)
     weight_decay = self.weight_decay_lambda * weight_decay / 2
-    y = self.predict(x)
+    y = self.predict(x, is_training)
     _, loss = self.lastLayer.forward(y, t)
     return loss + weight_decay
 
@@ -111,7 +131,7 @@ class MultiLayerNet:
     4
     """
     # forward
-    self.loss(x, t)
+    self.loss(x, t, is_training=True)
 
     # backward
     dout = self.lastLayer.backward(1)
@@ -122,7 +142,7 @@ class MultiLayerNet:
 
     # set gradient
     grads = {}
-    for i in range(1, int((len(self.layers) + 1) / 2) + 1):
+    for i in range(1, int(len(self.params) / 2) + 1):
       layer = self.layers["Affine{}".format(i)]
       grads["W{}".format(i)] = layer.dW + self.weight_decay_lambda * layer.W
       grads["b{}".format(i)] = layer.db
